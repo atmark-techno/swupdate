33924a5c93d00bd26164e5f5684e717b4a7cbdb9
Dominique Martinet <dominique.martinet@atmark-techno.com>
2023-04-14 10:19:39 +0900

    Atmark: add info message when installing each image
    mkswu will now store descriptions as properties, so swupdate can
    print a message as each part of the update is installed.
    This should make it more clear when each part is updated
    
    Change-Id: Id34f4e1f8ebf3c74a45fc52f32f466d755d48f0a

--- core/installer.c
+++ core/installer.c
@@ -244,6 +244,7 @@ int run_prepost_scripts(struct imglist *list, script_fn type)
 	int ret;
 	struct img_type *img;
 	struct installer_handler *hnd;
+	char *description;
 
 	/* Scripts must be run before installing images */
 	LIST_FOREACH(img, list, next) {
@@ -255,6 +256,11 @@ int run_prepost_scripts(struct imglist *list, script_fn type)
 				.scriptfn = type,
 				.data = hnd->data
 			};
+
+			description = dict_get_value(&img->properties, "description");
+			if (description) {
+				INFO("Installing %s", description);
+			}
 
 			swupdate_progress_inc_step(img->fname, hnd->desc);
 			swupdate_progress_update(0);
@@ -272,6 +278,7 @@ int run_prepost_scripts(struct imglist *list, script_fn type)
 int install_single_image(struct img_type *img, bool dry_run)
 {
 	struct installer_handler *hnd;
+	char *description;
 	int ret;
 
 	/*
@@ -287,6 +294,10 @@ int install_single_image(struct img_type *img, bool dry_run)
 		return -1;
 	}
 	TRACE("Found installer for stream %s %s", img->fname, hnd->desc);
+	description = dict_get_value(&img->properties, "description");
+	if (description) {
+		INFO("Installing %s", description);
+	}
 
 	swupdate_progress_inc_step(img->fname, hnd->desc);
 
