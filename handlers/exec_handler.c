cda53f43b319369efe184a526995c75d847fbdfa
Dominique Martinet <dominique.martinet@atmark-techno.com>
2023-05-30 16:56:45 +0900

    exec_handler: add property to preserve the original filename
    some commands might care about the filename of the command they
    run, so add a property and use that name if available
    
    Change-Id: I71dc7f12598fb2dfa39bba4ce14070a7ffbfcf41

--- handlers/exec_handler.c
+++ handlers/exec_handler.c
@@ -15,6 +15,34 @@
 #include "pctl.h"
 #include "util.h"
 
+static int move_to_original_name(struct img_type *img, char *fname) {
+	char *last_slash;
+	size_t str_size;
+
+	memcpy(img->path, img->extract_file, sizeof(img->path));
+	last_slash = strrchr(img->path, '/');
+	if (!last_slash)
+		return -EINVAL;
+	last_slash++;
+	str_size = sizeof(img->path) - (last_slash - img->extract_file) - 1;
+	if (snprintf(last_slash, str_size, "%s", fname) > str_size)
+		return -ERANGE;
+
+	/* Note this rename has the potential of overwriting a file
+	 * that would be used later; we don't actively support the
+	 * not-installed-directly pattern so let such users deal with that
+	 * (also, yes, fname can be ../../../etc/shadow, but we trust the
+	 * image anyway)
+	 */
+	if (rename(img->extract_file, img->path) != 0)
+		return -errno;
+
+	// update extract_file/fname for cleanup/other users
+	memcpy(img->extract_file, img->path, sizeof(img->extract_file));
+	strncpy(img->fname, fname, sizeof(img->fname));
+	return 0;
+}
+
 static int exec_image(struct img_type *img,
 	void __attribute__ ((__unused__)) *data)
 {
@@ -26,13 +54,16 @@ static int exec_image(struct img_type *img,
 		ERROR("Exec handler needs a command to run: please set the 'cmd' property");
 		return -EINVAL;
 	}
+	// original filename to use if possible
+	char *fname = dict_get_value(&img->properties, "filename");
 
 	if (img->install_directly) {
 		struct installer_handler *hnd;
 
 		// we need to extract the file ourselves, abuse rawfile handler
 		strlcpy(img->type, "rawfile", sizeof(img->type));
-		snprintf(img->path, sizeof(img->path), "%s/%s", get_tmpdir(), img->fname);
+		snprintf(img->path, sizeof(img->path), "%s%s", get_tmpdir(),
+			 fname ?:img->fname);
 
 		hnd = find_handler(img);
 		if (!hnd) {
@@ -44,6 +75,10 @@ static int exec_image(struct img_type *img,
 			return ret;
 		path = img->path;
 	} else {
+		if (fname && strcmp(img->fname, fname)) {
+			if (move_to_original_name(img, fname))
+				WARN("Could not preserve original file name, keeping current one");
+		}
 		path = img->extract_file;
 	}
 	if (asprintf(&cmd, "%s %s", cmd, path) < 0) {
