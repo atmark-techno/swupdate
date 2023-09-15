atmark-techno/swupdate

commit 8228b1390632195158f4387d7aae659ebd6f1469

Reorganize includes to have smaller files
Some header file like util.h is definetly grown, and this can generate
dependencies between file. Try to cleanup this and move some definitions
to specific file. While all modules require util.h, just a few of them
require specific definitions.

Signed-off-by: Stefano Babic <sbabic@denx.de>

include/util.h
@@ -12,12 +12,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
 #if defined(__linux__)
 #include <linux/types.h>
 #endif
-#include "swupdate.h"
+#include "globals.h"
 #include "swupdate_status.h"
-#include "swupdate_settings.h"
+#include "swupdate_dict.h"
 #include "compat.h"
 
 #define NOTIFY_BUF_SIZE 	2048
@@ -31,6 +32,12 @@
 
 #define HWID_REGEXP_PREFIX	"#RE:"
 #define SWUPDATE_ALIGN(A,S)    (((A) + (S) - 1) & ~((S) - 1))
+
+#define BOOTVAR_TRANSACTION "recovery_status"
+
+struct img_type;
+struct imglist;
+struct hw_type;
 
 extern int loglevel;
 extern int exit_code;
@@ -47,6 +54,13 @@ typedef enum {
 	SERVER_UPDATE_CANCELED,
 	SERVER_ID_REQUESTED,
 } server_op_res_t;
+
+enum {
+  COMPRESSED_FALSE,
+  COMPRESSED_TRUE,
+  COMPRESSED_ZLIB,
+  COMPRESSED_ZSTD,
+};
 
 /*
  * loglevel is used into TRACE / ERROR
@@ -206,11 +220,8 @@ char *substring(const char *src, int first, int len);
 char *string_tolower(char *s);
 size_t snescape(char *dst, size_t n, const char *src);
 void freeargs (char **argv);
-int get_hw_revision(struct hw_type *hw);
-void get_sw_versions(swupdate_cfg_handle *handle, struct swupdate_cfg *sw);
 int compare_versions(const char* left_version, const char* right_version);
 int hwid_match(const char* rev, const char* hwrev);
-int check_hw_compatibility(struct swupdate_cfg *cfg);
 int count_elem_list(struct imglist *list);
 unsigned int count_string_array(const char **nodes);
 void free_string_array(char **nodes);

core/Makefile
@@ -13,6 +13,7 @@ obj-y += swupdate.o \
 	 notifier.o \
 	 handler.o \
 	 bootloader.o \
+	 hw-compatibility.o \
 	 install_from_file.o \
 	 util.o \
 	 parser.o \

core/artifacts_versions.c
@@ -23,6 +23,7 @@
 #include "parselib.h"
 #include "swupdate_settings.h"
 #include "semver.h"
+#include "versions.h"
 
 /*
  * Read versions of components from a file, if provided

core/cpio_utils.c
@@ -20,6 +20,7 @@
 
 #include "generated/autoconf.h"
 #include "cpiohdr.h"
+#include "swupdate.h"
 #include "util.h"
 #include "sslapi.h"
 #include "progress.h"

core/hw-compatibility.c
@@ -1,0 +1,114 @@
+/*
+ * (C) Copyright 2023
+ * Stefano Babic, DENX Software Engineering, sbabic@denx.de.
+ *
+ * SPDX-License-Identifier:     GPL-2.0-only
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include "util.h"
+#include "swupdate.h"
+#include "hw-compatibility.h"
+
+/*
+ * The HW revision of the board *MUST* be inserted
+ * in the sw-description file
+ */
+#ifdef CONFIG_HW_COMPATIBILITY
+int check_hw_compatibility(struct swupdate_cfg *cfg)
+{
+	struct hw_type *hw;
+	int ret;
+
+	ret = get_hw_revision(&cfg->hw);
+	if (ret < 0)
+		return -1;
+
+	TRACE("Hardware %s Revision: %s", cfg->hw.boardname, cfg->hw.revision);
+	LIST_FOREACH(hw, &cfg->hardware, next) {
+		if (hw &&
+		    (!hwid_match(hw->revision, cfg->hw.revision))) {
+			TRACE("Hardware compatibility verified");
+			return 0;
+		}
+	}
+
+	return -1;
+}
+#else
+int check_hw_compatibility(struct swupdate_cfg
+		__attribute__ ((__unused__)) *cfg)
+{
+	return 0;
+}
+#endif
+
+/*
+ * This function is strict bounded with the hardware
+ * It reads some GPIOs to get the hardware revision
+ */
+int get_hw_revision(struct hw_type *hw)
+{
+	FILE *fp;
+	int ret;
+	char *b1, *b2;
+#ifdef CONFIG_HW_COMPATIBILITY_FILE
+#define HW_FILE CONFIG_HW_COMPATIBILITY_FILE
+#else
+#define HW_FILE "/etc/hwrevision"
+#endif
+
+	if (!hw)
+		return -EINVAL;
+
+	/*
+	 * do not overwrite if it is already set
+	 * (maybe from command line)
+	 */
+	if (strlen(hw->boardname))
+		return 0;
+
+	memset(hw->boardname, 0, sizeof(hw->boardname));
+	memset(hw->revision, 0, sizeof(hw->revision));
+
+	/*
+	 * Not all boards have pins for revision number
+	 * check if there is a file containing theHW revision number
+	 */
+	fp = fopen(HW_FILE, "r");
+	if (!fp)
+		return -1;
+
+	ret = fscanf(fp, "%ms %ms", &b1, &b2);
+	fclose(fp);
+
+	if (ret != 2) {
+		TRACE("Cannot find Board Revision");
+		if(ret == 1)
+			free(b1);
+		return -1;
+	}
+
+	if ((strlen(b1) > (SWUPDATE_GENERAL_STRING_SIZE) - 1) ||
+		(strlen(b2) > (SWUPDATE_GENERAL_STRING_SIZE - 1))) {
+		ERROR("Board name or revision too long");
+		ret = -1;
+		goto out;
+	}
+
+	strlcpy(hw->boardname, b1, sizeof(hw->boardname));
+	strlcpy(hw->revision, b2, sizeof(hw->revision));
+
+	ret = 0;
+
+out:
+	free(b1);
+	free(b2);
+
+	return ret;
+}
+
+

core/network_thread.c
@@ -30,6 +30,7 @@
 #include "installer.h"
 #include "installer_priv.h"
 #include "swupdate.h"
+#include "hw-compatibility.h"
 #include "pctl.h"
 #include "generated/autoconf.h"
 #include "state.h"

core/stream_interface.c
@@ -45,6 +45,7 @@
 #include "pctl.h"
 #include "state.h"
 #include "bootloader.h"
+#include "hw-compatibility.h"
 
 #define BUFF_SIZE	 4096
 #define PERCENT_LB_INDEX	4

core/swupdate.c
@@ -48,6 +48,8 @@
 #include "pctl.h"
 #include "state.h"
 #include "bootloader.h"
+#include "versions.h"
+#include "hw-compatibility.h"
 
 #ifdef CONFIG_SYSTEMD
 #include <systemd/sd-daemon.h>

commit a1089832332d80ec249f9f8146421f5bfd8db0fa

include/util.h: fix build on musl
Building on musl 1.2.4 fails with the following error/warnings:

include/util.h:211:23: error: unknown type name 'mode_t'
  211 | int mkpath(char *dir, mode_t mode);
include/util.h:269:36: warning: 'struct timeval' declared inside parameter list will not be visible outside of this definition or declaration
  269 | char *swupdate_time_iso8601(struct timeval *tv);

These were caused by a recent reorganization of the headers and glibc
appears more tolerant here.

Fixes: 8228b1390632 ("Reorganize includes to have smaller files")
Signed-off-by: Dominique Martinet <dominique.martinet@atmark-techno.com>

include/util.h
@@ -8,11 +8,13 @@
 #ifndef _UTIL_H
 #define _UTIL_H
 
+#include <fcntl.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <sys/time.h>
 #if defined(__linux__)
 #include <linux/types.h>
 #endif