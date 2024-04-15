e8cbbd3d3a5f64014000dff46c86e67fd949421b
Dominique Martinet <dominique.martinet@atmark-techno.com>
2024-04-15 10:56:23 +0900

    Atmark: swupdate: set SWUPDATE_VERSION environment variable
    scripts need to decide if the calling swpdate version is new enough to
    support something, export a variable so they can check
    
    Change-Id: I6041bc631be1ef70e4b62808354f81ecc4d3a231

--- core/swupdate.c
+++ core/swupdate.c
@@ -836,6 +836,10 @@ int main(int argc, char **argv)
 		}
 	}
 
+	setenv("SWUPDATE_VERSION",
+		STR(VERSION)"."STR(VERSION_PATCHLEVEL)"."STR(VERSION_SUBLEVEL)"-"STR(VERSION_EXTRAVERSION),
+		1);
+
 	printf("%s\n\n", BANNER);
 	printf("Licensed under GPLv2. See source distribution for detailed "
 		"copyright notices.\n\n");
