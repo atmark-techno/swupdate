afa66661a21b4df9c43d31561a17a411804dc3a8
Dominique Martinet <dominique.martinet@atmark-techno.com>
2024-04-17 07:22:49 +0900

    Atmark: mkswu: add locking as part of swupdate
    This will eventually allow us to remove the ugly mkdir locking we
    had in mkswu scripts, and more importantly will simplify C code for
    programs that want to take this lock.
    
    Change-Id: I9dcff4a5c3d80fb58e83f34e4fb8abd64788450b

--- core/stream_interface.c
+++ core/stream_interface.c
@@ -569,6 +569,8 @@ void *network_initializer(void *data)
 		stream_wkup = false;
 		inst.status = RUN;
 		pthread_mutex_unlock(&stream_mutex);
+		if (mkswu_lock())
+			ret = -EIO;
 		notify(START, RECOVERY_NO_ERROR, INFOLEVEL, "Software Update started !");
 		TRACE("Software update started");
 
@@ -609,7 +611,7 @@ void *network_initializer(void *data)
 		/*
 		 * Check if the stream should be saved
 		 */
-		if (!req->disable_store_swu  && strlen(software->output)) {
+		if (!ret && !req->disable_store_swu  && strlen(software->output)) {
 			ret = save_stream(inst.fd, software);
 			if (ret < 0) {
 				notify(FAILURE, RECOVERY_ERROR, ERRORLEVEL,
@@ -756,6 +758,8 @@ void *network_initializer(void *data)
 		swupdate_remove_directory(SCRIPTS_DIR_SUFFIX);
 		swupdate_remove_directory(DATADST_DIR_SUFFIX);
 #endif
+
+		mkswu_unlock();
 
 		/*
 		 * Last step, if no restart is required,
