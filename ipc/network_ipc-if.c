atmark-techno/swupdate

commit 8b4efc0d23dbf1cd9d7842073701933ebc58f61e

swupdate_async_start: fix early termination blocking in client
It could happen on the client side, that the termination status of the
installation was missed, leading to the client waiting forever.

This modification connects the progress socket before sending the image
payload, to be sure not to miss the termination status (SUCCESS or FAILURE).

We also need to consume the events during the image transfer, so that the pipe
does not get full and block the daemon.

Signed-off-by: Frederic Hoerni <fhoerni@witekio.com>

ipc/network_ipc-if.c
@@ -102,6 +102,54 @@ static void unstack_installation_status(getstatus callback)
 	} while (ipcmsg.data.status.current != IDLE);
 }
 
+/* Consume progress events
+ *
+ * Returns:
+ *        -1 error
+ *   FAILURE the installation failed
+ *   SUCCESS the installation suceeded
+ *         0 no event or other events consumed
+ *
+ * On error, progressfd is closed and set to -1.
+ */
+static int consume_progress_events(int *progressfd)
+{
+	struct progress_msg progressmsg;
+	int ret = -1;
+
+	/* Wait until the end of the installation (FAILURE or SUCCESS) */
+	while (1) {
+		int err = progress_ipc_receive_nb(progressfd, &progressmsg);
+		if (err < 0) {
+			/* Note that progressfd may have been closed by progress_ipc_receive
+			 * and set to -1 */
+			fprintf(stderr, "progress_ipc_receive_nb failed (%d)\n", ret);
+			ret = -1;
+			break;
+		} else if (err == 0) {
+			/* no pending message */
+			ret = 0;
+			break;
+		}
+
+		if (progressmsg.status == FAILURE || progressmsg.status == SUCCESS) {
+			/* We have the final result of the installation */
+			ret = progressmsg.status;
+			break;
+		} else {
+			/* Other status (START, RUN, PROGRESS) */
+			/* continue consuming messages */
+			continue;
+		}
+	}
+
+	if (ret == -1 && *progressfd >= 0) {
+		close(*progressfd);
+		*progressfd = -1;
+	}
+
+	return ret;
+}
 
 static void *swupdate_async_thread(void *data)
 {
@@ -113,6 +161,8 @@ static void *swupdate_async_thread(void *data)
 	struct async_lib *rq = (struct async_lib *)data;
 	int swupdate_result = FAILURE;
 	int progressfd = -1;
+	int ret;
+	int early_status = -1;
 
 	sigemptyset(&sigpipe_mask);
 	sigaddset(&sigpipe_mask, SIGPIPE);
@@ -122,6 +172,16 @@ static void *swupdate_async_thread(void *data)
 		swupdate_result = FAILURE;
 		goto out;
 	}
+	/* Start listening to progress events, before sending
+	 * the image so that we don't miss the result event.
+	 */
+	progressfd = progress_ipc_connect(0 /* no reconnect */);
+	if (progressfd < 0) {
+		fprintf(stderr, "progress_ipc_connect failed\n");
+		ipc_end(rq->connfd);
+		goto out;
+	}
+
 	/* Start writing the image until end */
 
 	do {
@@ -136,26 +196,40 @@ static void *swupdate_async_thread(void *data)
 				goto out;
 			}
 		}
+		/* Consume progress events so that the pipe does not get full
+		 * and block the daemon */
+		ret = consume_progress_events(&progressfd);
+		if (ret == -1) {
+			/* If we cannot get events, then we won't be able to get the result.
+			 * Quit and fail */
+			fprintf(stderr, "Cannot consume progress events. Fail.\n");
+			early_status = FAILURE;
+			break;
+		} else if (ret == FAILURE || ret == SUCCESS) {
+			/* early termination */
+			fprintf(stderr, "early termination while sending the image: %s\n",
+			        ret==SUCCESS?"SUCCESS":"FAILURE");
+			early_status = ret;
+			/* interrupt the transfer */
+			break;
+		}
 	} while(size > 0);
 
-	/* Start listening to progress events, before ipc_end
-	 * so that we don't miss the result event.
-	 */
-	progressfd = progress_ipc_connect(0 /* no reconnect */);
- 
 	ipc_end(rq->connfd);
-
-	if (progressfd < 0) {
-		fprintf(stderr, "progress_ipc_connect failed\n");
-		goto out;
-	}
 
 	/*
 	 * Everything sent, wait for completion of the installation
 	 */
 
-	/* Wait until the end of the installation and get the final result */
-	swupdate_result = inst_wait_for_complete(&progressfd); /* progressfd closed by the call */
+	if (early_status >= 0) {
+		swupdate_result = early_status;
+		close(progressfd);
+		progressfd = -1;
+
+	} else {
+		/* Wait until the end of the installation and get the final result */
+		swupdate_result = inst_wait_for_complete(&progressfd); /* progressfd closed by the call */
+	}
 
 	/*
 	 * Get and print all status lines, for compatibility with legacy programs

commit 7cd09f4e343590353b4072b4fb82277c28fc74d1

swupdate_async_start: fix hang with update containing many small steps
Running swupdate with a SWU (streamed through swupdate_async_start such
as with `swupdate -i` or `swupdate-client`) can hang if the update
contains many small steps followed by a large step:
- swupdate_async_thread() is stuck trying to write the SWU content to
  the server
- but the server sent many progress events and filled progressfd
  backlog, and closes the server side of in send_progress_msg() after
  retries failed (client is still blocked)
- when server continued installing more items the client is unblocked
  and reads up on progress notifications, notices the socket is closed
  (POLLHUP) and fails
- the server fails the update as it is not complete

This fails with logs similar to this:
```
progress_ipc_receive_nb failed (-1)
Cannot consume progress events. Fail.
[ERROR] : SWUPDATE failed [0] ERROR install_from_file.c : endupdate : 55 : SWUpdate *failed* !
[TRACE] : SWUPDATE running : [unlink_sockets] : unlink socket /tmp/swupdateprog
[TRACE] : SWUPDATE running : [unlink_sockets] : unlink socket /tmp/sockinstctrl
[ERROR] : SWUPDATE failed [0] ERROR cpio_utils.c : hash_compare : 521 : HASH mismatch : 4395313e6f79b6554521e1933304e95d0bea836d2bc72e9b3828fd62c5125baf <--> 3eb039c6666dfd614a73b930c98c38ab77ba362ce3b9d745c7684d0d10335726
```

This patch makes the sending socket nonblocking, allows partial sends of
SWU content (there is no need for it to be a full write unlike IPC
messages), and polls both progressfd and connfd to handle both sockets
simultaneously.

The poll is somewhat redundant with the other poll on progressfd in
progress_ipc_receive_nb(), but progress_ipc_receive_nb() is part of the
progress_ipc API and cannot be changed, so is left as is.

Fixes: 8b4efc0d23db ("swupdate_async_start: fix early termination blocking in client")
Link: https://groups.google.com/g/swupdate/c/F3XKUdP0KTc/m/m2jk4EdkBgAJ
Signed-off-by: Dominique Martinet <dominique.martinet@atmark-techno.com>

ipc/network_ipc-if.c
@@ -5,13 +5,15 @@
  * SPDX-License-Identifier:     LGPL-2.1-or-later
  */
 
+#include <errno.h>
+#include <fcntl.h>
+#include <inttypes.h>
+#include <poll.h>
+#include <pthread.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
-#include <signal.h>
-#include <pthread.h>
-#include <inttypes.h>
 #include <unistd.h>
 #include "network_ipc.h"
 #include "progress_ipc.h"
@@ -155,14 +157,15 @@ static int consume_progress_events(int *progressfd)
 static void *swupdate_async_thread(void *data)
 {
 	char *pbuf;
-	int size;
+	int size = 0, buf_offset = 0;
+	struct pollfd pfds[2];
 	sigset_t sigpipe_mask;
 	sigset_t saved_mask;
 	struct timespec zerotime = {0, 0};
 	struct async_lib *rq = (struct async_lib *)data;
 	int swupdate_result = FAILURE;
 	int progressfd = -1;
-	int ret;
+	int ret, nfd, old_flags;
 	int early_status = -1;
 
 	sigemptyset(&sigpipe_mask);
@@ -183,18 +186,48 @@ static void *swupdate_async_thread(void *data)
 		goto out;
 	}
 
+	/* make connfd non-blocking while we're writing image */
+	old_flags = fcntl(rq->connfd, F_GETFL);
+	if (old_flags < 0) {
+		fprintf(stderr, "Could not get connfd flags? %m\n");
+		goto out;
+	}
+	fcntl(rq->connfd, F_SETFL, old_flags | O_NONBLOCK);
+
 	/* Start writing the image until end */
 
 	do {
 		if (!rq->wr)
 			break;
+		if (size == buf_offset) {
+			buf_offset = 0;
+			rq->wr(&pbuf, &size);
+		}
 
-		rq->wr(&pbuf, &size);
 		if (size) {
-			if (swupdate_image_write(pbuf, size) != size) {
-				perror("swupdate_image_write failed");
-				swupdate_result = FAILURE;
+			pfds[0].fd = progressfd;
+			pfds[0].events = POLLIN;
+			pfds[1].fd = rq->connfd;
+			pfds[1].events = POLLOUT;
+			nfd = 2;
+			do {
+				ret = poll(pfds, nfd, -1);
+			} while (ret < 0 && errno == EINTR);
+
+			if (ret < 0) {
+				fprintf(stderr, "poll failed: %m\n");
 				goto out;
+			}
+			if (pfds[1].revents & POLLOUT) {
+				ret = swupdate_image_write(pbuf + buf_offset,
+							   size - buf_offset);
+				if (ret < 0 && errno != EAGAIN) {
+					perror("swupdate_image_write failed");
+					swupdate_result = FAILURE;
+					goto out;
+				} else if (ret > 0) {
+					buf_offset += ret;
+				}
 			}
 		}
 		/* Consume progress events so that the pipe does not get full
@@ -214,7 +247,7 @@ static void *swupdate_async_thread(void *data)
 			/* interrupt the transfer */
 			break;
 		}
-	} while(size > 0);
+	} while (size > 0);
 
 	ipc_end(rq->connfd);
 

ipc/network_ipc.c
@@ -320,6 +320,11 @@ int ipc_send_data(int connfd, char *buf, int size)
 
 	while (len) {
 		ret = write(connfd, buf, (size_t)len);
+		if (ret < 0 && errno == EINTR)
+			continue;
+		/* return partially written length if any */
+		if (ret < 0 && errno == EAGAIN && len != size)
+			return size - len;
 		if (ret < 0)
 			return ret;
 		len -= ret;